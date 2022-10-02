d001fe17b0cfaffd6ccc15a85ac781cdbc95abe5
David <davidrus.art@gmail.com>
2022-10-02 14:46:06 +0300

    add exception handling

---
+++ src/main/java/com/motomania/motoshop/exceptionhandler/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.motomania.motoshop.exceptionhandler;
+
+import lombok.Data;
+
+@Data
+public class ErrorCode {
+
+    private String code;
+    private String message;
+
+    public ErrorCode(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
